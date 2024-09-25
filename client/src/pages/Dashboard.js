2c578b1a8cc1b607241421476c11f5a0573579ea
37Bronze <ndinhdong.1808@gmail.com>
2024-09-25 14:10:04 +0700

    update

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import Swal from 'sweetalert2';
 import io from 'socket.io-client';
 import Cards from '../components/Cards';
 import Clock from '../components/Clock';
 import Led from '../components/Led';
 import DataChart from '../components/DataChart';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Dashboard = ({
     temp,
@@ -43,7 +45,49 @@ const Dashboard = ({
     const [lastAlertTime, setLastAlertTime] = useState(0);
     const [lastLightAlertTime, setLastLightAlertTime] = useState(0);
 
-    
+    const [relay1, setRelay1] = useState(null);
+    const [relay2, setRelay2] = useState(null);
+    const [relay3, setRelay3] = useState(null);
+
+    useEffect(() => {
+        const socket = io('http://localhost:8688');
+
+        // Listen for updates from the server
+        socket.on('relay_1', (data_received) => {
+            if (relay1 !== null && relay1 !== data_received) {
+                toast(`Đèn đã được ${data_received == '1' ? 'bật' : 'tắt'}`, {
+                    autoClose: 5000,
+                    position: "bottom-right"
+                });
+            }
+            setRelay1(data_received);
+        });
+
+        socket.on('relay_3', (data_received) => {
+            if (relay3 !== null && relay3 !== data_received) {
+                toast(`Điều hòa đã được ${data_received == '1' ? 'bật' : 'tắt'}`, {
+                    autoClose: 5000,
+                    position: "bottom-right"
+                });
+            }
+            setRelay3(data_received);
+        });
+
+        socket.on('relay_2', (data_received) => {
+            if (relay2 !== null && relay2 !== data_received) {
+                toast(`Quạt đã được ${data_received == '1' ? 'bật' : 'tắt'}`, {
+                    autoClose: 5000,
+                    position: "bottom-right"
+                });
+            }
+            setRelay2(data_received);
+        });
+
+        // Clean up the socket listener when the component unmounts
+        return () => {
+            socket.disconnect();
+        };
+    }, [relay1, relay2, relay3]);
 
     // Hàm cảnh báo nhiệt độ cao
     useEffect(() => {
@@ -79,10 +123,6 @@ const Dashboard = ({
 
     return (
         <div>
-            {/* <strong className="h-[90px] border-b mr-[100px] flex justify-start items-center">
-                <h1>DASHBOARD</h1>
-                <Clock />
-            </strong> */}
             <hr className="mr-[100px]" />
 
             <div className="mr-[100px] mt-10">
@@ -134,6 +174,8 @@ const Dashboard = ({
                     />
                 </div>
             </div>
+
+            <ToastContainer />
         </div>
     );
 };
